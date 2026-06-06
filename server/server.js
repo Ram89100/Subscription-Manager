397ffdd044938185b72ae5e61181577f19847582
Ram89100 <ram89100@gmail.com>
2026-06-06 23:50:55 +0530

    Add cron job

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
-const app = require('./app'); 
+const app = require('./app');
+const { startRenewalReminderCron } = require('./cron/renewalReminder');
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  
+  // Start cron jobs
+  startRenewalReminderCron();
 });
